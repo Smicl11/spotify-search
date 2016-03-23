f8728d8751aa4ac94eb66a2a80632791196e5338
smicl11 <bryan.smith094@gmail.com>
2016-03-22 20:26:55 -0700

    first version. search not working.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,66 @@ $(document).on('ready', function() {
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
-  // your code here
+  var $spotifySearch = $('#spotify-search');
+
+  var $track = $('#track');
+
+  var $results = $('#results');
+
+  var $loading = $('#loading');
+
+  $spotifySearch.on('submit', function handleFormSubmit(event) {
+    even.preventDefault();
+
+    $results.empty();
+
+    var searchTrack = $track.val();
+
+    if (searchTrack !== "") {
+      $loading.show();
+
+      var searchUrl = 'https://api.spotify.com/v1/search?type=track&q=' + searchTrack;
+
+      $.ajax({
+        method: 'GET',
+        url: searchUrl,
+        success: handleSpotifyData
+      });
+    } else {
+      alert("You need to search for something first!");
+    }
+
+    $spotifySearch[0].reset();
+    $track.focus();
+  });
+
+  function handleSpotifyData(data) {
+    var trackResults = data.tracks.items;
+
+    $loading.hide();
+
+    if (trackResults.length > 0) {
+      trackResults.forEach(function (results, index) {
+        var trackData = {
+          albumArt: results.album.images.length > 0 ? results.album.images[0].url : null,
+          artist: results.artists[0].name,
+          name: results.name,
+          previewUrl: results.preview_url
+        };
+
+        var $trackHtml = '<div class="row"><div class="col-xs-4">' +
+          '<img src="' + trackData.albumArt + '" class="img-responsive"></div>' +
+          '<div class="col-xs-8"><p><strong>' + trackData.name + '</strong> by ' +
+          trackData.artist + '</p><p><a href="' + trackData.previewUrl +
+          '" target="_blank" class="btn btn-sm btn-default">Preview ' +
+          '<span class="glyphicon glyphicon-play"></span></a></p></div></div><hr>';
+
+          $results.append($trackHtml);
+      });
+
+    } else {
+      $results.append('<p class="text-center">Nothing Here</p>');
+    }
+  }
 
 });
